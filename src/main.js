e1246020a8904b20dbdba38cb639c2237d91e746
iksadnorth <iksadnorth@gmail.com>
2022-07-14 03:15:58 +0900

    install & configure axios for backend

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.config.globalProperties.$axios = axios;
 app.use(router).mount('#app')

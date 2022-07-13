f16433d29c11e378334aa8feb6912c97e7c6969a
iksadnorth <iksadnorth@gmail.com>
2022-07-14 00:39:10 +0900

    implement header & footer of insta

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,44 @@
 <template>
-    <div></div>
+    <ul>
+      <li v-for="item in upside" :key="item">{{item}}</li>
+    </ul>
+    <ul>
+      <li v-for="item in downside" :key="item">{{item}}</li>
+    </ul>
+    <ul>
+      <li>
+        <select name="language" id="" v-model="language_selected">
+          <option :value="i" v-for="(item, i) in language" :key="i">{{item}}</option>
+        </select>
+      </li>
+      <li>
+         2022 Instagram from Meta
+      </li>
+    </ul>
+    
 </template>
 
 <script>
 export default {
-    name: 'insta_footer'
+    name: 'insta_footer',
+    data() {
+      return {
+        upside: ["Meta", "소개", "블로그", "채용 정보", "도움말", "API", "개인정보처리방침", "약관", "인기 계정", "해시태그", "위치", "Instagram Lite", "연락처 업로드 & 비사용자"],
+        downside: ["댄스", "음식집 및 정원", "음악", "시각 예술"],
+        language: ["한국어", "en"],
+        language_selected: 0
+      }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
-  list-style-type: none;
-  padding: 0;
+  text-align: center;
 }
 li {
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  margin-right: 20px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
-  <a href=""><img src="../assets/logo.png" alt=""></a>
-  <b-container class="bv-example-row">
-    <b-row>
-      <b-col>1 of 3</b-col>
-      <b-col>2 of 3</b-col>
-      <b-col>3 of 3</b-col>
-    </b-row>
-</b-container>
+  <div class="container">
+    <div class="col"></div>
+    <div class="col">
+      <a href=""><img src="../assets/logo.png" alt=""></a>
+    </div>
+    <div class="col">
+      <input type="검색">
+    </div>
+    <div class="col">
+      <button><img src="../assets/icon_home.png" alt=""></button>
+      <button><img src="../assets/icon_fly.png" alt=""></button>
+      <button><img src="../assets/icon_plus.png" alt=""></button>
+      <button><img src="../assets/icon_compass.png" alt=""></button>
+      <button><img src="../assets/icon_heart.png" alt=""></button>
+      <button><img src="../assets/icon_account.png" alt=""></button>
+    </div>
+    <div class="col"></div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +27,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .container {
+    display: flex;
+  }
+  .container > .col {
+    flex: 1;
+    margin: auto;
+  }
 </style>
